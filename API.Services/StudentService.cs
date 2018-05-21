1d31c2d2fa97a1a7d09465db52b65e9388ae1e47
Vikas Sharma <mailbox.viksharma@gmail.com>
2018-05-21 12:01:00 +0530

    proj target framework changed

--- API.Services/StudentService.cs
+++ API.Services/StudentService.cs
@@ -4,9 +4,6 @@ using API.Entities;
 using Dto = API.DTO.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace API.Services
 {
